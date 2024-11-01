e6a7b99341a25260ffca77dac9085058fdfa36e1
sarthak4399 <sarthakkhandare21@gmail.com>
2024-11-01 17:12:56 +0530

    71A_solution

--- practice/71_waytooLongWord/solution.cpp
+++ practice/71_waytooLongWord/solution.cpp
@@ -1,1 +1,27 @@
 // Solution for 71_waytooLongWord
+#include<iostream>
+#include<vector>
+using namespace std ;
+class Solutions{
+    public:
+        string abrivation(string word){
+            if(word.length()> 10 ){
+                return string(1, word[0]) + to_string(word.length() - 2) + word[word.length() - 1];
+            }else{
+                return word ;
+            }
+        }
+};
+
+int main() {
+    Solutions s ;
+    int n;
+    cin >> n;
+    vector<string> words(n);
+    for (int i = 0; i < n; i++) {
+        cin >> words[i];
+    }
+    for (const string& word : words) {
+        cout << s.abrivation(word) << endl;
+    }
+}
